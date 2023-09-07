ee572936148a13a19fa9b480945eacaff38e0127
Ana <anaagr30@gmail.com>
2023-09-07 15:27:59 +0800

    first commit

---
+++ Toogle/test.js
@@ -0,0 +1,8 @@
+function babylaugh() {
+    document.getElementById("imgStatus").src = "https://i.makeagif.com/media/10-03-2017/zhzEC1.gif";
+    document.getElementById("mp3File").play();
+}
+function babysmile() {
+    document.getElementById("imgStatus").src ="https://cdn.webshopapp.com/shops/286980/files/328562443/2000x1500x1/image.jpg";
+    document.getElementById("mp3File").pause();
+}
